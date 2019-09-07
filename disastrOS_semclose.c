f1d10b49cbb0e22ba719acdb00dbdff8221d08dd
Christian <31599295+chrismarinoni@users.noreply.github.com>
2019-09-07 11:30:02 +0200

    SemClose v1
    Just a first version that needs lots of improvements.

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -8,5 +8,60 @@
 
 void internal_semClose(){
 	
-  // do stuff :)
+	int fd = running->syscall_args[0];
+	
+	SemDescriptor* semDesc = SemDescriptorList_byFd(&running->sem_descriptors, fd);
+	
+	if(!semDesc){
+		printf("[SEM_ERR] Trying to close a non-existing semaphore!\n");
+		running->syscall_retvalue = DSOS_ESEMCLOSE_NOT_EXIST;
+		return;
+	}
+	
+	List_detach(&running->sem_descriptors, (ListItem*) semDesc); 
+	
+
+	Semaphore* sem = semDesc->semaphore;
+	
+	if(!sem){
+		printf("[SEM_ERR] I wasn't able to get the semaphore to destroy!\n");
+		running->syscall_retvalue = DSOS_ESEMCLOSE_NOT_FOUND;
+		return;
+	}
+	
+	List_detach(&semaphores_list, (ListItem*) sem);
+	
+	int ret;
+	
+	if(sem->descriptors.size == 0 && sem->waiting_descriptors.size == 0){
+		ret = Semaphore_free(sem);
+		if(ret == 0){
+			printf("[SEM_ERR] Semaphore_free failed!\n");
+		}
+	}
+	
+	SemDescriptorPtr* semDescPtr = semDesc->ptr;
+	
+	if(!semDescPtr){
+		printf("[SEM_ERR] I wasn't able to get the semDescPtr!\n");
+		running->syscall_retvalue = DSOS_ESEMCLOSE_NOT_FOUND;
+		return;
+	}
+		
+	List_detach(&running->descriptors, (ListItem*) semDescPtr);
+	
+	
+	
+	
+	
+	
+	
+	
+	if(ret != 0){
+		printf("[SEM_ERR] Semaphore_free failed! Error Code: %d - see PoolAllocatorResult for more details.\n", ret);
+		running->syscall_retvalue = DSOS_ESEMCLOSE_FAILED;
+		return;
+	}
+		
+	
 }
