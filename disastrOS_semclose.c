b145ebc63b6dd7653b8612198390d960e33afa00
Christian <31599295+chrismarinoni@users.noreply.github.com>
2019-09-10 22:19:10 +0200

    Some errors partially solved
    Solved a problem that was causing a segmentation fault when detaching semDescPtr in the SemClose. Further changes are needed to solve a problem with  Semaphore_free.

--- disastrOS_semclose.c
+++ disastrOS_semclose.c
@@ -6,6 +6,8 @@
 #include "disastrOS_semaphore.h"
 #include "disastrOS_semdescriptor.h"
 
+#define DEBUG 1
+
 void internal_semClose(){
 	
 	int fd = running->syscall_args[0];
@@ -18,35 +20,51 @@ void internal_semClose(){
 		return;
 	}
 	
-	List_detach(&running->sem_descriptors, (ListItem*) semDesc); 
+	List_detach(&running->sem_descriptors, (ListItem *)semDesc);
 	
+	if(DEBUG) printf("Test 1\n");
 
 	Semaphore* sem = semDesc->semaphore;
-	
-	if(!sem){
+		
+	if(sem==0){
 		printf("[SEM_ERR] I wasn't able to get the semaphore to destroy!\n");
 		running->syscall_retvalue = DSOS_ESEMCLOSE_NOT_FOUND;
 		return;
 	}
 	
-	List_detach(&semaphores_list, (ListItem*) sem);
-	
-	int ret;
-	
-	if(sem->descriptors.size == 0 && sem->waiting_descriptors.size == 0){
-		ret = Semaphore_free(sem);
-		if(ret == 0){
-			printf("[SEM_ERR] Semaphore_free failed!\n");
-		}
-	}
-	
-	SemDescriptorPtr* semDescPtr = semDesc->ptr;
-	
+	SemDescriptorPtr* semDescPtr = (SemDescriptorPtr*)List_detach(&sem->descriptors, (ListItem*)(semDesc->ptr));
+					
 	if(!semDescPtr){
 		printf("[SEM_ERR] I wasn't able to get the semDescPtr!\n");
 		running->syscall_retvalue = DSOS_ESEMCLOSE_NOT_FOUND;
 		return;
 	}
+
+	List_detach(&running->sem_descriptors, (ListItem*) semDesc); 
+	List_detach(&semaphores_list, (ListItem*) sem);
+	
+	if(DEBUG) printf("Test 2\n");
+	
+	int ret;
+	
+	SemDescriptor_free(semDesc);
+	SemDescriptorPtr_free(semDescPtr);
+
+	
+	if(sem->descriptors.size == 0 && sem->waiting_descriptors.size == 0){
+		
+		ret = Semaphore_free(sem);
+		
+		if(DEBUG) printf("Test 3\n");
+		
+		if(ret == 0){
+			printf("[SEM_ERR] Semaphore_free failed!\n");
+		}
+	}
+	
+	//SemDescriptorPtr* semDescPtr = semDesc->ptr;
+	
+	
 		
 	List_detach(&running->descriptors, (ListItem*) semDescPtr);
 	
