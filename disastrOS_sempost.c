f2be8afd20b30ada407e294063e5aed6cf77de84
Christian <31599295+chrismarinoni@users.noreply.github.com>
2019-09-12 18:48:24 +0200

    SemPost: 1st version
    Code compiled with no errors. The test program must be modified to verify that the code is correct also on the functional side. Further work is therefore required.

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -7,5 +7,41 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semPost(){
-  // do stuff :)
+  
+  int fd = running->syscall_args[0];
+	
+	SemDescriptor* semDesc = SemDescriptorList_byFd(&running->sem_descriptors, fd);
+	
+	if(!semDesc){
+		printf("[SEM_ERR] Trying to close a non-existing semaphore!\n");
+		running->syscall_retvalue = DSOS_ESEMWAIT_NOT_EXIST;
+		return;
+	}
+	
+	Semaphore* sem = semDesc->semaphore;
+		
+	if(!sem){
+		printf("[SEM_ERR] I wasn't able to get the semaphore to destroy!\n");
+		running->syscall_retvalue = DSOS_ESEMWAIT_NOT_FOUND;
+		return;
+	}
+	
+	SemDescriptorPtr* semDescPtr = semDesc->ptr;
+					
+	if(!semDescPtr){
+		printf("[SEM_ERR] I wasn't able to get the semDescPtr!\n");
+		running->syscall_retvalue = DSOS_ESEMWAIT_NOT_FOUND;
+		return;
+	}
+	
+	
+	if(++sem->count <= 0) {
+		List_insert(&ready_list, ready_list.last, (ListItem*) running);
+		List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*)semDescPtr);
+		List_detach(&waiting_list, (ListItem*)semDesc->pcb);
+		running->status = Ready;
+		running = semDesc->pcb;
+		
+	}
+  
 }
